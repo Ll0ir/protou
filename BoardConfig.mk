7dee0a69386872137b3f49bc2c1ac6a33f02c59f
Ll0ir <rob_shannon539@hotmail.com>
2012-10-24 00:53:42 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,37 +8,30 @@ TARGET_BOARD_PLATFORM := unknown
 TARGET_CPU_ABI := armeabi
 TARGET_BOOTLOADER_BOARD_NAME := protou
 
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=protou
-BOARD_KERNEL_BASE := 0x13F00000
-BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
+BOARD_KERNEL_BASE := 0x03200000
+BOARD_KERNEL_PAGESIZE := 2048
 
 # fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x080000000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x0010FE80000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x63BFFC0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x79FFFC0000
-BOARD_FLASH_BLOCK_SIZE := 1310722
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/htc/protou/kernel
 
-# This is needed so CWM will function properly
-
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
-BOARD_SDEXT_DEVICE := /dev/block/mmcblk0p32
-BOARD_USES_MMCUTILS := false
-BOARD_HAS_NO_MISC_PARTITION := false
-
-BOARD_HAS_LARGE_FILESYSTEM := true
+#BOARD_HAS_NO_SELECT_BUTTON := true
+# Use this flag if the board has a ext4 partition larger than 2gb
+#BOARD_HAS_LARGE_FILESYSTEM := true
 
 # Actual UMS different from TWRP for some reason
 
-BOARD_UMS_LUNFILE := /sys/class/android_usb/f_mass_storage/lun0/file
+#BOARD_UMS_LUNFILE := /sys/class/android_usb/f_mass_storage/lun0/file
 
 #Lun File testing
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_otg/msm_hsusb/gadget/lun1/file
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_otg/msm_hsusb/gadget/lun2/file
-
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_otg/msm_hsusb/gadget/lun1/file
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_otg/msm_hsusb/gadget/lun2/file
 
 # Script
 
